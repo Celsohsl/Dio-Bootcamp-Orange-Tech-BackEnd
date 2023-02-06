8022335727ea67977571413392c860bbe96e1aee
Celsohsl <celsohsl@icloud.com>
2023-02-05 23:33:06 -0300

    Alterando conteúdo da classe Fatorial

--- loops-e-arrays/src/loops/Fatorial.java
+++ loops-e-arrays/src/loops/Fatorial.java
@@ -23,7 +23,14 @@ public class Fatorial {
 		}
 		
 		System.out.println(multiplicacao);
+		
+		int[][] numeros = {{4, 9, 8, 5}, {3, 0, 1, 5}, {1, 2, 7, 4}};
+
+		System.out.print(numeros[2][2]);
+
+        }
+
 
 	}
 
-}
+
