e836e407d60c47e066110f438a256e4b2f55b2f8
Celsohsl <celsohsl@icloud.com>
2023-02-07 15:00:54 -0300

    Adicionando conteúdos na classe Carro

--- programacao-orientada-objetos/src/estrutura/basica/poo/Carro.java
+++ programacao-orientada-objetos/src/estrutura/basica/poo/Carro.java
@@ -1,5 +1,11 @@
 package estrutura.basica.poo;
 
+//classe de exemplo sobre poo
 public class Carro {
+
+	// criando atributos para classe carro:
+	String cor;
+	String modelo;
+	int capacidadeTanque;
 
 }
