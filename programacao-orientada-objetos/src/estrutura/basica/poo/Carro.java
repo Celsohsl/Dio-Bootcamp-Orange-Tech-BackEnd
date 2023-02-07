74624a6a27505628ebf8debd6ab2b59c7326010c
Celsohsl <celsohsl@icloud.com>
2023-02-07 16:22:52 -0300

    Adicionando conteúdo na classe Carro

--- programacao-orientada-objetos/src/estrutura/basica/poo/Carro.java
+++ programacao-orientada-objetos/src/estrutura/basica/poo/Carro.java
@@ -3,9 +3,52 @@ package estrutura.basica.poo;
 //classe de exemplo sobre poo
 public class Carro {
 
-	// criando atributos para classe carro:
+	// criando atributos para classe carro
 	String cor;
 	String modelo;
 	int capacidadeTanque;
+
+	// criando construtor vazio
+	Carro() {
+
+	}
+
+	// criando o construtor com parâmetros
+	Carro(String cor, String modelo, int capacidadeTanque) {
+		this.cor = cor;
+		this.modelo = modelo;
+		this.capacidadeTanque = capacidadeTanque;
+	}
+
+	// gerando get e set
+	void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	String getCor() {
+		return cor;
+	}
+
+	void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	String getModelo() {
+		return modelo;
+	}
+
+	void setCapacidadeTanque(int capacidadeTanque) {
+		this.capacidadeTanque = capacidadeTanque;
+	}
+
+	int getCapacidadeTanque() {
+		return capacidadeTanque;
+	}
+
+	// método do valor para encher o tanque
+	double totalValorTanque(double valorCombustivel) {
+		return capacidadeTanque * valorCombustivel;
+
+	}
 
 }
