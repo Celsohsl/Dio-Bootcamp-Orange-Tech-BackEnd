661716615dce5c77328595fbdcbb23fe3fca6133
Celsohsl <celsohsl@icloud.com>
2023-02-08 16:53:02 -0300

    Alterando a classe RodarAplicacao

--- programacao-orientada-objetos/src/heranca/associacao/polimorfismo/RodarAplicacao.java
+++ programacao-orientada-objetos/src/heranca/associacao/polimorfismo/RodarAplicacao.java
@@ -7,19 +7,19 @@ public class RodarAplicacao {
 		Mae[] classes = new Mae[] { new ClasseFilha1(), new ClasseFilha2(), new Mae()
 
 		};
-		
-		for(Mae classe: classes) {
+
+		for (Mae classe : classes) {
 			classe.metodo1();
 		}
-		
+
 		System.out.println("");
-		
-		for(Mae classe: classes) {
+
+		for (Mae classe : classes) {
 			classe.metodo2();
 		}
-		
+
 		System.out.println("");
-		
+
 		ClasseFilha2 classeFilha2 = new ClasseFilha2();
 		classeFilha2.metodo2();
 
