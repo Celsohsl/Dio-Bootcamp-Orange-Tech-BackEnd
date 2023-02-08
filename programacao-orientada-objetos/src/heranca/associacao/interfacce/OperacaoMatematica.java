0d0572cefda2e4e0a1bdf8842ebf5dbf7d8ed53e
Celsohsl <celsohsl@icloud.com>
2023-02-08 16:54:44 -0300

    Criando pacote, adicionado classes

---
+++ programacao-orientada-objetos/src/heranca/associacao/interfacce/OperacaoMatematica.java
@@ -0,0 +1,13 @@
+package heranca.associacao.interfacce;
+
+interface OperacaoMatematica {
+
+	void soma(double operando1, double operando2);
+
+	void subtracao(double operando1, double operando2);
+
+	void multiplicacao(double operando1, double operando2);
+
+	void divisao(double operando1, double operando2);
+
+}
