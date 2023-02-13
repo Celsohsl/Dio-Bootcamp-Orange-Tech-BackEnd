43fbfcebc08200d9882f9f3217bebb9e05fdc070
Celsohsl <celsohsl@icloud.com>
2023-02-13 15:34:59 -0300

    Adicionando novo projeto collections

---
+++ trabalhando-com-collections-java/src/collections/list/MetodoListExemplo.java
@@ -0,0 +1,49 @@
+package collections.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MetodoListExemplo {
+
+	public static void main(String[] args) {
+		
+		//List notas = new ArrayList(); //usado antes do java 5
+		//List<Double> notas = new ArrayLst<>(); //Generics - Diamond Operator
+		//ArrayList<Double> notas = new ArrayList<>();
+		//List<Double> notas = new ArrayList<>(Array.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+		
+		/*List<Double> notas = Array.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+		notas.add(10d);
+		System.out.println(notas);*/
+		
+		System.out.println("Crie uma lista e adicione as sete notas");
+		List<Double> notas = new ArrayList<Double>();
+		notas.add(7.0);
+		notas.add(8.5);
+		notas.add(9.3);
+		notas.add(5.0);
+		notas.add(7.0);
+		notas.add(0.0);
+		notas.add(3.6);
+		
+		System.out.println(notas.toString());
+		
+		System.out.println("Exiba a posição da no 5.0: " + notas.indexOf(5d));
+		
+		System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+		notas.add(4, 8d);
+		System.out.println(notas);
+		
+		System.out.println("Substitua a nota 5.0 pela nota 7.0: ");
+		notas.set(notas.indexOf(5d), 7.0);
+		System.out.println(notas);
+		
+		System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
+		
+		System.out.println("Exiba todas as notas na ordem em que foram informadas: ");
+		for (Double nota : notas) System.out.println(nota);
+			
+		
+	}
+
+}
